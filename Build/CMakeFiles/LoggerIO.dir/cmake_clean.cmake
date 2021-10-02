8870cc2512dfe91e3a954d9a81947c9c68a94078
Melvin Devos <melvin.devos@student.vives.be>
2021-10-02 16:51:46 +0100

    Basic input reading

---
+++ Build/CMakeFiles/LoggerIO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LoggerIO.dir/main.c.o"
+  "LoggerIO"
+  "LoggerIO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LoggerIO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
