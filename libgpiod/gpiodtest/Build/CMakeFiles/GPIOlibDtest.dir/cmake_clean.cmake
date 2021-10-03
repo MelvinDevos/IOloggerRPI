5dff91dd8f0a92827de934e603c9e0db99a55549
Melvin Devos <melvin.devos@student.vives.be>
2021-10-03 15:16:55 +0100

    Opdracht uitbreiding libgpiod

---
+++ libgpiod/gpiodtest/Build/CMakeFiles/GPIOlibDtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GPIOlibDtest.dir/main.c.o"
+  "GPIOlibDtest"
+  "GPIOlibDtest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GPIOlibDtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
